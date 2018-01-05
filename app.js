98770612fa1b1ccb48f45f468aa3991cc887da68
KrishnaGapileswar <kabikrish2797@gmail.com>
2018-01-05 21:41:45 +0530

    altered as a name calculator app

--- app.js
+++ app.js
@@ -5,12 +5,25 @@
 
 
   //angular.module returns an object
-  angular.module('myFirstApp', [])
+  angular.module('nameApp', [])
 
-  .controller('myFirstController', function(){
+  .controller('nameController', function($scope){
+    $scope.name = "";
+    $scope.numValue = 0;
 
+    $scope.getNumValue = function(){
+      $scope.numValue = calculateValue($scope.name);
+    };
+
+    function calculateValue(string){
+      var numValue = 0;
+      for(var i=0;i<string.length;i++){
+        numValue += string.charCodeAt(i);
+      }
+      return numValue;
+    }
 
   });
 
-  
+
 })();
