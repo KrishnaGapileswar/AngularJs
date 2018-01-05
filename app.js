f3517d4b61bb32c101aa79ac37db9c35fc524ead
KrishnaGapileswar <kabikrish2797@gmail.com>
2018-01-05 21:09:31 +0530

    angular.min.js file added

---
+++ app.js
@@ -0,0 +1,16 @@
+(function (){
+
+  'use strict';
+
+
+
+  //angular.module returns an object
+  angular.module('myFirstApp', [])
+
+  .controller('myFirstController', function(){
+
+
+  });
+
+  
+})();
